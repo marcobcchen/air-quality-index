688003888035bfdc2ef2d0dffb8363fdb3556524
marcobcchen <marco.chen@hngf.com.tw>
2020-07-21 15:24:46 +0800

    Updates

---
+++ precache-manifest.e44621ada30b8a582b7121d4b76a0a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88348e160ba124f7bb510ce5f2a244a0",
+    "url": "/air-quality-index/index.html"
+  },
+  {
+    "revision": "94c8146481953441a756",
+    "url": "/air-quality-index/static/css/main.20f774eb.chunk.css"
+  },
+  {
+    "revision": "d43925ce61b90a256d30",
+    "url": "/air-quality-index/static/js/2.909761d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/air-quality-index/static/js/2.909761d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c8146481953441a756",
+    "url": "/air-quality-index/static/js/main.68c70d5a.chunk.js"
+  },
+  {
+    "revision": "42c87a35d109b4a4b625",
+    "url": "/air-quality-index/static/js/runtime-main.cac851e4.js"
+  }
+]);
